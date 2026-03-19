ead377adb3cef1ba5968ff28526925253559257d
mohammadblack666 <blackmohammad5@gmail.com>
2026-03-19 03:43:13 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,47 +1,23 @@
-/* تحميل السجلات */
+alert("admin.js LOADED");
+
 const loadBtn = document.getElementById("loadLogs");
 const clearBtn = document.getElementById("clearLogs");
-const logsBox = document.getElementById("logsBox");
 const logoutBtn = document.getElementById("logoutBtn");
 
 if (loadBtn) {
     loadBtn.addEventListener("click", () => {
-        logsBox.innerHTML = "";
-
-        let logs = JSON.parse(localStorage.getItem("logs") || "[]");
-
-        if (logs.length === 0) {
-            logsBox.innerHTML = "<p>No logs found.</p>";
-            return;
-        }
-
-        logs.forEach((log, index) => {
-            logsBox.innerHTML += 
-                <div class="log-entry">
-                    <p><b>User:</b> ${log.user}</p>
-                    <p><b>Zone:</b> ${log.zone}</p>
-                    <p><b>From:</b> ${log.from}</p>
-                    <p><b>How:</b> ${log.how}</p>
-                    <p><b>Sent by:</b> ${log.sent}</p>
-                    <p><b>Time:</b> ${log.time}</p>
-                    <hr>
-                </div>
-            ;
-        });
+        alert("LOAD LOGS clicked");
     });
 }
 
-/* حذف السجلات */
 if (clearBtn) {
     clearBtn.addEventListener("click", () => {
-        localStorage.removeItem("logs");
-        logsBox.innerHTML = "<p>Logs cleared.</p>";
+        alert("CLEAR LOGS clicked");
     });
 }
 
-/* تسجيل الخروج */
 if (logoutBtn) {
     logoutBtn.addEventListener("click", () => {
-        window.location.href = "login.html";
+        alert("LOG OUT clicked");
     });
 }
